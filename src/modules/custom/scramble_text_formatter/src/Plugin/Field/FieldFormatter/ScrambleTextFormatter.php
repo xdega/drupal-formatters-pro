bcec636f4522d8f04c10b8d9c4996e4ddd477e0f
Liam Hockley <xdega121@gmail.com>
2020-11-05 12:21:15 -0800

    Implement first working formatter.

---
+++ src/modules/custom/scramble_text_formatter/src/Plugin/Field/FieldFormatter/ScrambleTextFormatter.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Drupal\scramble_text_formatter\Plugin\Field\FieldFormatter;
+
+use Drupal\Core\Field\FormatterBase;
+use Drupal\Core\Field\FieldItemListInterface;
+
+/**
+ * Plugin implementation of the 'text_format' formatter.
+ *
+ * @FieldFormatter(
+ *   id = "field_formatter_scramble_text",
+ *   label = @Translation("Scramble Text Field"),
+ *   field_types = {
+ *     "string",
+ *     "text",
+ *   }
+ * )
+ */
+
+class ScrambleTextFormatter extends FormatterBase
+{
+  public function viewElements(FieldItemListInterface $items, $langcode)
+  {
+    $elements = [];
+
+    foreach ($items as $index => $item) {
+      $result = str_shuffle($item->value);
+      $elements[$index] = ["#markup" => $result];
+    }
+
+    return $elements;
+  }
+}
